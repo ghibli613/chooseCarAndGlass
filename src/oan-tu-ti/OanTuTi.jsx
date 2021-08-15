5b9642d5b4fa6c547824cd757065361a0146b212
duongy96@gmail.com <duongy96@gmail.com>
2021-08-15 22:21:43 +0700

    mentor-review

--- src/oan-tu-ti/OanTuTi.jsx
+++ src/oan-tu-ti/OanTuTi.jsx
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
 import './OanTuTi.css';
 import ThongTinGame from './ThongTinGame';
-import { connect } from "react-redux";
-import {
-    actOanTuTi,
-    actPlayGame,
-} from '../store/actions/oanTuTiActions';
+import { connect } from 'react-redux';
+import { actOanTuTi, actPlayGame } from '../store/actions/oanTuTiActions';
 
+const listSelect = [
+    {
+        type: 'keo',
+        src: 'oanTuTi-img/keo.png',
+    },
+    {
+        type: 'bua',
+        src: 'oanTuTi-img/bua.png',
+    },
+    {
+        type: 'bao',
+        src: 'oanTuTi-img/bao.png',
+    },
+];
 class OanTuTi extends Component {
     render() {
-        const {me, you, oanTuTi, playGame} = this.props;
+        const { me, you, oanTuTi, playGame } = this.props;
 
         return (
-            <div className='game-container'>
-                <div className='container-fluid'>
+            <div className="game-container">
+                <div className="container-fluid">
                     <div className="row text-center">
                         <div className="col-4 d-block">
-                            <div className='left'>
-                                <img src={`./oanTuTi-img/${me}.png`} width='60px'/>
+                            <div className="left">
+                                <img src={`./oanTuTi-img/${me}.png`} width="60px" />
                             </div>
-                            <img src="./oanTuTi-img/player.png" height='150px'/>
+                            <img src="./oanTuTi-img/player.png" height="150px" />
                             <div className="d-flex align-items-center justify-content-center">
-                                <button className={`btnOanTuTi ${me==='keo'&& "btnSelect"}`} onClick={() => oanTuTi('keo')}>
+                                {/* khúc này cách em tạo điều kiện để add class rất tốt,
+                                    nhưng a bổ sung thêm cách để generate các button để cho sạch code, để dữ liệu được động hơn
+                                */}
+                                {listSelect.map((item, key) => (
+                                    <button
+                                        key={'btn-' + key}
+                                        className={`btnOanTuTi ${me === item.type && 'btnSelect'}`}
+                                        onClick={() => oanTuTi(item.type)}
+                                    >
+                                        <img src={item.src} />
+                                    </button>
+                                ))}
+                                {/* <button className={`btnOanTuTi ${me==='keo'&& "btnSelect"}`} onClick={() => oanTuTi('keo')}>
                                     <img src="./oanTuTi-img/keo.png"/>
                                 </button>
                                 <button className={`btnOanTuTi ${me==='bua'&& "btnSelect"}`} onClick={() => oanTuTi('bua')}>
@@ -29,24 +52,26 @@ class OanTuTi extends Component {
                                 </button>
                                 <button className={`btnOanTuTi ${me==='bao'&& "btnSelect"}`} onClick={() => oanTuTi('bao')}>
                                     <img src="./oanTuTi-img/bao.png"/>
-                                </button>
+                                </button> */}
                             </div>
                         </div>
                         <div className="col-4 py-5">
-                            <div className='fact'>I'm iron man, i love you 3000!!</div>
-                            <ThongTinGame/>
-                            <button className='mt-5 btn-success' onClick={() => playGame()}>Play Game</button>
+                            <div className="fact">I'm iron man, i love you 3000!!</div>
+                            <ThongTinGame />
+                            <button className="mt-5 btn-success" onClick={() => playGame()}>
+                                Play Game
+                            </button>
                         </div>
                         <div className="col-4">
-                            <div className='right'>
-                                <img src={`./oanTuTi-img/${you}.png`} width='60px'/>
+                            <div className="right">
+                                <img src={`./oanTuTi-img/${you}.png`} width="60px" />
                             </div>
-                            <img src="./oanTuTi-img/playerComputer.png" height='150px'/>
+                            <img src="./oanTuTi-img/playerComputer.png" height="150px" />
                         </div>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
--- src/store/reducers/oanTuTiReducer.js
+++ src/store/reducers/oanTuTiReducer.js
@@ -1,11 +1,13 @@
 import * as oanTuTiConstants from '../constants/oanTuTiConstants';
 
 const initialState = {
+    // cần bổ sung thêm 1 state result nữa là thắng, hòa hoặc thua để hiển thị
+    // result được xử lý theo thuật toán bên dưới
     me: 'keo',
     soBanThang: 0,
     soBanChoi: 0,
     you: 'keo',
-}
+};
 
 const oanTuTiReducer = (state = initialState, { type, payload }) => {
     switch (type) {
@@ -14,27 +16,43 @@ const oanTuTiReducer = (state = initialState, { type, payload }) => {
 
             return { ...state };
         case oanTuTiConstants.PLAY_GAME:
-
             // Tăng tổng số bàn chơi 1 đơn vị mỗi khi bấm Play Game
             state.soBanChoi += 1;
 
-            // Random 
+            // Random
             const soNgauNhien = Math.floor(Math.random() * 3) + 1;
 
-            if(soNgauNhien === 1) {
+            if (soNgauNhien === 1) {
                 state.you = 'keo';
-            } else if(soNgauNhien === 2) {
+            } else if (soNgauNhien === 2) {
                 state.you = 'bua';
             } else {
                 state.you = 'bao';
             }
+            // thuật toán để check thắng hòa thua như sau:
+            // tạo ra 1 list [keo,bua,bao]; mỗi lần người dùng chọn hoặc máy chọn thì dùng hàm findIndex để lấy index ra
+            // để ý thấy cái mảng này, thì index của nó có quy luật thắng thua như sau
+            // mình chọn cái gì bạn chọn đúng cái đó thì hòa, cái này dễ
+            // mình chọn bất cứ cái gì, nếu bạn chọn sau mình 1 index, mình thua ("bua" thắng "keo", "bao" thang "bua"),
+            // hoặc bạn chọn trước mình 2 đơn vị mình cũng thua(mình chọn "bao" bạn chọn "keo")
+            // if(indexYou===indexMe){
+            //     result ="DRAW";
+            // }else if(indexMe===indexYou-1|| indexMe === indexYou+2){
+            //     result ="LOSE";
+            // }else{
+            //     result ="WIN";
+            // }
 
             // Kiểm tra số bàn thắng
-            if ((state.me === 'keo' && state.you === 'bao') || (state.me === 'bua' && state.you === 'keo') || (state.me === 'bao' && state.you === 'bua')) {
+            if (
+                (state.me === 'keo' && state.you === 'bao') ||
+                (state.me === 'bua' && state.you === 'keo') ||
+                (state.me === 'bao' && state.you === 'bua')
+            ) {
                 state.soBanThang += 1;
             }
 
-            return { ...state};
+            return { ...state };
         default:
             return state;
     }
